283d3958fcfa267e980e06422031d967b6aa1752
KeadenM <keadenmeacham@gmail.com>
2023-08-02 09:39:24 -0600

    finished models

---
+++ models/Thought.js
@@ -0,0 +1,22 @@
+const {Schema, Types} = require('mongoose');
+const Reaction = require('./Reaction');
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [Reaction]
+});
+
+modult.exports = thoughtSchema;
---
+++ models/User.js
@@ -0,0 +1,19 @@
+const {Schema, Types} = require('mongoose');
+const {Thought} = require('./Thought');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type:String,
+        unique: true,
+        required: true,
+        match: [/.+@.+\..+/]
+    },
+});
+
+module.exports = userSchema;
