a5dbc1d8fb08e7fe8208cdd018bdfd6a7ed47279
KeadenM <keadenmeacham@gmail.com>
2023-08-07 18:33:42 -0600

    bug fixes and added thought routing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,58 +1,66 @@
 const { User } = require('../models');
 
-module.export = {
-    getUsers(req, res) {
-        User.find()
-            .then((users) => res.json(users))
-            .catch((err)=> res.status(500).json(err));
-    
+module.exports = {
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
     },
-}
-//     getSingleUser(req, res) {
-//         try{
-//             const user = User.findone({ _id: req.params.id });
-//             res.json(user);
-//         }
-//         catch(err){
-//             res.status(500).json(err);
-//         }
-//     },
-    
-//     createUser(req, res) {
-//             try{
-//                 const user = User.create(req.body);
-//                 res.json(user);
-//             }
-//             catch(err){
-//                 res.status(500).json(err);
-//             }
-//         },
-//     updateUser(req, res) {
-//             try{
-//                 const user = User.findOneAndUpdate(
-//                     { _id: req.params.id },
-//                     {$set: req.body},
-//                     {runValidators: true,}
-//                 )
-//                 res.json(user);
-//             }
-//             catch(err){
-//                 res.status(500).json(err);
-//             }
-//         },
-//     deleteUser(req, res) {
-//         try{
-//             const user = User.findOneAndDelete({ _id: req.params.id });
-//             if(!user){
-//                 res.status(404).json({message: "No user found with this id!"});
-//                 return;
-//             }
-//             res.json({message: "User deleted!"});
-//         }
-//         catch(err){
-//             res.status(500).json(err);
-//         }
-//     }
-// }
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+                .select('-__v');
 
- // module.exports = userController;
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    async updateUser(req, res) {
+        try{
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!user) {
+                res.status(404).json({ message: 'User not found' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+            if (!user) {
+                res.status(404).json({ message: 'User not found'});
+            }
+
+            res.json({ message: "User deleted!"})
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+};
+
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,6 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-    return res.send('Wrong Route!');
-});
+router.use('/api', apiRoutes);
 
 module.exports = router;
